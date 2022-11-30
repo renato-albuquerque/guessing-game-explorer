cd33bf3bf7836ae7a54f199c321c7adf674e7b53
renato-albuquerque <renatomalb83@gmail.com>
2022-11-30 10:19:16 -0300

    project updated

--- main.js
+++ main.js
@@ -26,12 +26,17 @@ const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
-const randomNumber = Math.round(Math.random() * 10)
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
 // Events
 btnTry.addEventListener("click", handleTryClick)
 btnReset.addEventListener("click", handleResetClick)
+document.addEventListener("keydown", function(e) {
+    if(e.key == "Enter" && screen1.classList.contains("hide")) {
+        handleResetClick()
+    }
+})
 
 // Callback Function
 function handleTryClick(event) {
@@ -53,6 +58,7 @@ function handleTryClick(event) {
 function handleResetClick() {
     toggleScreen()
     xAttempts = 1
+    randomNumber = Math.round(Math.random() * 10)
 }
 
 function toggleScreen() {
